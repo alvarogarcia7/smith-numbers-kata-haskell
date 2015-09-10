e0e187ea6bf4421cbdf49cfc506207b579d13b75
alvaro garcia <derecerca@gmail.com>
2015-09-10 22:29:04 +0200

    green: decompose in factors

--- Spec.hs
+++ Spec.hs
@@ -20,16 +20,28 @@ main = hspec $ do
         it "should sum four digits" $ do
 	  sumDigits 1234 `shouldBe` 10
 
+    describe "factorization in primes" $ do
+        it "corner cases" $ do
+	  factors 0 `shouldBe` []
+	  factors 1 `shouldBe` []
+
+        it "should decompose a prime" $ do
+	  factors 2 `shouldBe` [2]
+
+        it "should decompose a non-prime" $ do
+	  factors 6 `shouldBe` [2, 3]
+
 
 sumDigits :: Integer -> Integer
 sumDigits n = (n `mod` 10) + if n >= 10 then (sumDigits (n `div` 10)) else 0
 
 isPrime n = length [i|i<-[2..n], n `mod` i == 0] == 1
 
-factors :: Integer -> [[Integer]]
-factors n = [[i,n `div` i]|i<-[2..n], n `mod` i == 0, isPrime i] 
+factors :: Integer -> [Integer]
+factors n = reverse $ factors' n []
 
 factors' :: Integer -> [Integer] -> [Integer]
+factors' 0 primes = []
 factors' 1 primes = primes 
 factors' n current = factors' (n `div` i) (i:current)
     where i = head [i|i<-[head' current 2..n], n `mod` i == 0, isPrime i]
