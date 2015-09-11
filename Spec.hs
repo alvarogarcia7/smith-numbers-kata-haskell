c7e167326590a5f3eb7856b98575c7701cfa95d2
alvaro garcia <derecerca@gmail.com>
2015-09-11 08:11:42 +0200

    add canary test for quickcheck

--- Spec.hs
+++ Spec.hs
@@ -2,7 +2,9 @@ import Test.Hspec
 import Test.QuickCheck
 
 main :: IO ()
-main = hspec $ do
+main = do
+  quickCheck prop_true
+  hspec $ do
     describe "Canary Test" $ do
         it "should be green" $ do
           True `shouldBe` True
@@ -53,6 +55,9 @@ main = hspec $ do
 	it "should detect all OEIS Smith numbers" $ do
 	  filter (\x->x == False) (map (\x -> isSmith x) [4, 22, 27, 58, 85, 94, 121, 166, 202, 265, 274, 319, 346, 355, 378, 382, 391, 438, 454, 483, 517, 526, 535, 562, 576, 588, 627, 634, 636, 645, 648, 654, 663, 666, 690, 706, 728, 729, 762, 778, 825, 852, 861, 895, 913, 915, 922, 958, 985, 1086, 1111, 1165]) `shouldBe` []
 
+
+prop_true :: Bool -> Bool -> Bool
+prop_true _ _ = True
 
 sumDigits :: Integer -> Integer
 sumDigits 0 = 0
