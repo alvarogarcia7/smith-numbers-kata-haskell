4663e6b52636df4a4366fc41430d4f2e7203174f
alvaro garcia <derecerca@gmail.com>
2015-09-11 09:25:02 +0200

    add test for first property

--- Spec.hs
+++ Spec.hs
@@ -56,6 +56,10 @@ main = do
 	it "should detect all OEIS Smith numbers" $ do
 	  filter (\x->x == False) (map (\x -> isSmith x) [4, 22, 27, 58, 85, 94, 121, 166, 202, 265, 274, 319, 346, 355, 378, 382, 391, 438, 454, 483, 517, 526, 535, 562, 576, 588, 627, 634, 636, 645, 648, 654, 663, 666, 690, 706, 728, 729, 762, 778, 825, 852, 861, 895, 913, 915, 922, 958, 985, 1086, 1111, 1165]) `shouldBe` []
 
+    describe "properties about numbers" $ do
+        it "n should be n>=sum_digits n" $ do
+          (filter (not . id) $ map property_sumDigits_ge_sumDigits_factors_n  [2..11000]) `shouldBe` []
+
 
 sumDigits :: Integer -> Integer
 sumDigits 0 = 0
